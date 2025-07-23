a5a92e6be0b6e14bbff2f8e15e10d4af9c37be44
Aastick Sharma <aasticksharma0@gmail.com>
2025-07-23 22:59:16 +0530

    Fix: removed old submodule reference and re-added frontend as normal folder

---
+++ Frontend/src/components/About.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { HiOutlineArrowRight } from "react-icons/hi";
+
+const About = () => {
+  return (
+    <>
+      <section className="about" id="about">
+        <div className="container">
+          <div className="banner">
+            <div className="top">
+              <h1 className="heading">ABOUT US</h1>
+              <p>The only thing we're serious about is food.</p>
+            </div>
+            <p className="mid">
+              Welcome to [KHAO PIYO], where flavor meets tradition. 
+              We serve delicious, freshly prepared meals using locally 
+              sourced ingredients. Whether you're here for a quick bite 
+              or a cozy dinner, our warm ambiance and friendly service 
+              ensure a memorable experience. Taste the difference. Visit us today!
+            </p>
+            <Link to={"/"}>
+              Explore Menu{" "}
+              <span>
+                <HiOutlineArrowRight />
+              </span>
+            </Link>
+          </div>
+          <div className="banner">
+            <img src="about.png" alt="about" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
